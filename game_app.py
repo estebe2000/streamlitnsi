4a922dbcdca7fc48bf50d132409bb87fffe07b1a
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-10-19 11:22:51 +0000

    titre

--- game_app.py
+++ game_app.py
@@ -5,14 +5,9 @@ st.title("Exemple d'iframe dans Streamlit")
 
 url = "https://kitao.github.io/pyxel/wasm/launcher/?run=estebe2000.streamlitnsi.app"
 
-# Récupérer le contenu HTML de la page
-response = requests.get(url)
-content = response.text
-
-# Extraire le titre de la page en recherchant la balise <title>
-start_index = content.find("<title>") + len("<title>")
-end_index = content.find("</title>", start_index)
-titre = content[start_index:end_index] if start_index != -1 and end_index != -1 else "Titre non disponible"
+a = url.split("=")
+b = a[1].split(".")
+titre = b[0]
 
 # Afficher le titre
 st.subheader(f"Titre : {titre}")
