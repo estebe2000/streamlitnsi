5996c1fa81e753fc6860f5e976cd8e58a6b84007
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-10-19 11:09:09 +0000

    send

--- game_app.py
+++ game_app.py
@@ -5,7 +5,7 @@ st.title("Exemple d'iframe dans Streamlit")
 # Afficher une iframe
 #iframe_code = """<iframe src="https://kitao.github.io/pyxel/wasm/launcher/?run=estebe2000.streamlit.appp" width="800" height="600"></iframe>"""
 #st.markdown(iframe_code, unsafe_allow_html=True)
-url = "st.markdown(f'<iframe src="{url}" width="800" height="600"></iframe>', unsafe_allow_html=True)"
+url = "https://kitao.github.io/pyxel/wasm/launcher/?run=estebe2000.streamlit.appp"
 st.markdown(f'<iframe src="{url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
 
 
