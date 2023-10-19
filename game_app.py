1393774543f2c706f4cfe5c15d5aaf3eb9060c5c
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-10-19 11:19:21 +0000

    validatz

--- game_app.py
+++ game_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
 st.title("Exemple d'iframe dans Streamlit")
 
-# Afficher une iframe
 url = "https://kitao.github.io/pyxel/wasm/launcher/?run=estebe2000.streamlitnsi.app"
-st.markdown(f'<iframe src="{url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
 
+# Récupérer le contenu HTML de la page
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extraire le titre de la page
+titre = soup.title.string if soup.title else "Titre non disponible"
+
+# Afficher le titre
+st.subheader(f"Titre : {titre}")
+
+# Afficher l'iframe
+st.markdown(f'<iframe src="{url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
